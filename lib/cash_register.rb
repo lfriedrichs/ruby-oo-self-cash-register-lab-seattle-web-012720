e69334712564c1512e185c542e283636cdf074d5
github username <github email address>
2020-01-28 18:05:57 -0800

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
+class CashRegister 
 
+    attr_accessor :total, :discount
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @cart = Array.new
+    end
+
+    def add_item(item, price, quantity=1)
+        @total += price * quantity 
+        quantity.times do
+            @cart.push(item)
+        end
+        @last_item_price = price * quantity
+    end
+
+    def apply_discount
+        if @discount == 0
+            "There is no discount to apply."
+        else
+            @total = @total - @total * @discount / 100 
+            "After the discount, the total comes to $#{@total}."
+        end
+    end
+
+    def items 
+        @cart
+    end
+
+    def void_last_transaction
+        @total -= @last_item_price
+    end
+end
